505d4cf304fa56731b1c14e732e3b61ab2ff0e14
Monirith-huot <hmonirith@gmail.com>
2023-02-20 08:19:39 +0700

    finish

--- lib/src/presentation/views/progress_ordering/progress_ordering.screen.dart
+++ lib/src/presentation/views/progress_ordering/progress_ordering.screen.dart
@@ -1,3 +1,4 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 
 class ProgressOrderingScreen extends StatefulWidget {
@@ -14,38 +15,80 @@ class _ProgressOrderingScreenState extends State<ProgressOrderingScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       //1
-      body: CustomScrollView(
-        slivers: <Widget>[
-          //2
-          SliverAppBar(
-            floating: true,
-            pinned: true,
-            snap: false,
-            expandedHeight: 250.0,
-            flexibleSpace: FlexibleSpaceBar(
-              title: Text('Goa', textScaleFactor: 1),
-              background: Image.network(
-                'https://images.unsplash.com/photo-1676799911060-72c098f7ad3d?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1335&q=80',
-                fit: BoxFit.fill,
+      body: StreamBuilder<DocumentSnapshot>(
+        stream: FirebaseFirestore.instance
+            .collection("users")
+            .doc(widget.userId)
+            .snapshots(),
+        builder:
+            (BuildContext context, AsyncSnapshot<DocumentSnapshot> snapshot) {
+          if (snapshot.hasError) {
+            return Text('Something went wrong');
+          }
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return Text("Loading");
+          }
+          List ordering = [];
+          if (snapshot.hasData) {
+            Map<String, dynamic>? data =
+                snapshot.data?.data() as Map<String, dynamic>?;
+            ordering = data?['ordering'];
+          }
+          print(ordering[0]['status']);
+          return CustomScrollView(
+            slivers: <Widget>[
+              //2
+              SliverAppBar(
+                // floating: false,
+                pinned: true,
+                // snap: false,
+                expandedHeight: 150.0,
+                flexibleSpace: FlexibleSpaceBar(
+                            title: Text('Goa', textScaleFactor: 1),
+                            background: ordering[0]['status'] == "ORDERING"
+                                ? Image.asset(
+                                    'assets/images/waiting_driver.png',
+                                    fit: BoxFit.fill,
+                                  )
+                                : ordering[0]['status']) ==
+                        "COOKING"
+                    ? Image.asset(
+                        'assets/images/cooking.png',
+                        fit: BoxFit.fill,
+                      )
+                    : ordering[0]['status'] == "DELIVERY"
+                        ? Image.asset(
+                            'assets/images/delivering_to.png',
+                            fit: BoxFit.fill,
+                          )
+                        : ordering[0]['status'] == "FINISH"
+                            ? Image.asset(
+                                'assets/images/delivered.png',
+                                fit: BoxFit.fill,
+                              )
+                            : Image.asset(
+                                'assets/images/waiting_driver.png',
+                                fit: BoxFit.fill,
+                              ),
               ),
-            ),
-          ),
-          //3
-          SliverList(
-            delegate: SliverChildBuilderDelegate(
-              (_, int index) {
-                return ListTile(
-                  leading: Container(
-                      padding: EdgeInsets.all(8),
-                      width: 100,
-                      child: Placeholder()),
-                  title: Text('Place ${index + 1}', textScaleFactor: 2),
-                );
-              },
-              childCount: 20,
-            ),
-          ),
-        ],
+              //3
+              SliverList(
+                delegate: SliverChildBuilderDelegate(
+                  (_, int index) {
+                    return ListTile(
+                      leading: Container(
+                          padding: EdgeInsets.all(8),
+                          width: 100,
+                          child: Placeholder()),
+                      title: Text('Place ${index + 1}', textScaleFactor: 2),
+                    );
+                  },
+                  childCount: 20,
+                ),
+              ),
+            ],
+          );
+        },
       ),
     );
   }
--- lib/src/presentation/views/search/search.screen.dart
+++ lib/src/presentation/views/search/search.screen.dart
@@ -1,3 +1,4 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 
 import 'package:food_delivery_app/src/presentation/customize.dart';
@@ -15,34 +16,60 @@ class _SearchScreenState extends State<SearchScreen> {
   final restaraunName = TextEditingController();
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: COLORS.primary,
-        title: Container(
-          width: double.infinity,
-          height: 40,
-          decoration: BoxDecoration(
-            color: Colors.white,
-            borderRadius: BorderRadius.circular(5),
-          ),
-          child: Center(
-            child: TextField(
-              controller: restaraunName,
-              decoration: InputDecoration(
-                prefixIcon: const Icon(Icons.search),
-                suffixIcon: IconButton(
-                  icon: const Icon(Icons.clear),
-                  onPressed: () {
-                    print(restaraunName.text);
-                  },
+    return StreamBuilder<QuerySnapshot>(
+      stream: FirebaseFirestore.instance.collection("restaurant ").snapshots(),
+      builder: (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot) {
+        if (snapshot.hasError) {
+          return Text('Something went wrong');
+        }
+        if (snapshot.connectionState == ConnectionState.waiting) {
+          return Text("Loading");
+        }
+
+
+
+        return Scaffold(
+          appBar: AppBar(
+            backgroundColor: COLORS.primary,
+            title: Container(
+              width: double.infinity,
+              height: 40,
+              decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.circular(5),
+              ),
+              child: Theme(
+                data: ThemeData(
+                  colorScheme: ThemeData().colorScheme.copyWith(
+                        secondary: COLORS.primary,
+                      ),
                 ),
-                hintText: 'Search for your favorite restaurant',
-                border: InputBorder.none,
+                child: Center(
+                  child: TextField(
+                    onSubmitted: (restaraunName) {
+                      print(restaraunName);
+                    },
+                    controller: restaraunName,
+                    decoration: InputDecoration(
+                      prefixIcon: const Icon(
+                        Icons.search,
+                      ),
+                      suffixIcon: IconButton(
+                        icon: const Icon(Icons.clear),
+                        onPressed: () {
+                          restaraunName.clear();
+                        },
+                      ),
+                      hintText: 'Search for your favorite restaurant',
+                      border: InputBorder.none,
+                    ),
+                  ),
+                ),
               ),
             ),
           ),
-        ),
-      ),
+        );
+      },
     );
   }
 }
